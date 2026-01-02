5b9ec0f930da0bc07f1e3f0d17ed3adbddb532a7
Edward Sun <edward.sun2015@gmail.com>
2026-01-02 00:23:13 -0800

    clean up and draft style

--- build-site.el
+++ build-site.el
@@ -16,7 +16,10 @@
 (setq org-html-validation-link nil            ;; Don't show validation link
       org-html-head-include-scripts nil       ;; Use our own scripts
       org-html-head-include-default-style nil ;; Use our own styles
-      org-html-head "<link rel='stylesheet' href='https://cdn.jsdelivr.net/npm/spcss@0.9.0'>")
+      org-html-head "<link rel='stylesheet' href='https://cdn.jsdelivr.net/npm/spcss@0.9.0'>\n<link rel='stylesheet' href='/site.css'>")
+
+;; Keep publish cache local to avoid permission issues in shared dirs.
+(setq org-publish-timestamp-directory "./.org-timestamps/")
 
 ;; Define the publishing project
 (setq org-publish-project-alist
@@ -30,7 +33,13 @@
              :with-creator t            ;; Include Emacs and Org versions in footer
              :with-toc nil                ;; Include a table of contents
              :section-numbers nil       ;; Don't include section numbers
-             :time-stamp-file t)))    ;; Don't include time stamp in file
+             :time-stamp-file t)
+       (list "org-site:assets"
+             :recursive t
+             :base-directory "./assets"
+             :base-extension "png\\|jpg\\|jpeg\\|gif\\|svg\\|webp"
+             :publishing-directory "./public/assets"
+             :publishing-function 'org-publish-attachment)))
 
 ;; Generate the site output (don't use cache files, regenerate all)
 (org-publish-all t)
